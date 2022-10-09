22320c31b4aa427dd93400d04a8d78a9e95db8fc
khang nguyen <keirames.l3@gmail.com>
2022-10-09 23:53:12 +0700

    wip: simple worker pool

--- main.go
+++ main.go
@@ -21,10 +21,13 @@ func getQueueUrl(sess *session.Session, queue string) (*sqs.GetQueueUrlOutput, e
 }
 
 func getMessages(sess *session.Session, queueUrl string, maxMessages int) (*sqs.ReceiveMessageOutput, error) {
+	fmt.Println("fetch messages")
 	msgRes, err := sqs.New(sess).ReceiveMessage(&sqs.ReceiveMessageInput{
 		QueueUrl:            &queueUrl,
-		MaxNumberOfMessages: aws.Int64(1),
+		MaxNumberOfMessages: aws.Int64(10),
+		WaitTimeSeconds:     aws.Int64(20),
 	})
+	fmt.Println("got", len(msgRes.Messages))
 
 	if err != nil {
 		return nil, err
@@ -33,16 +36,16 @@ func getMessages(sess *session.Session, queueUrl string, maxMessages int) (*sqs.
 	return msgRes, nil
 }
 
-func worker(tasks <-chan int, results chan<- int) {
-	fmt.Println("Worker initialize...")
+func worker(id int, tasks <-chan string, results chan<- string) {
 	for task := range tasks {
-		time.Sleep(time.Second)
-		fmt.Println("Done task", task)
+		fmt.Println("Worker", id, "started")
+		if id == 1 {
+			time.Sleep(time.Second * 20)
+		} else {
+		}
+		fmt.Println("Worker", id, "done")
 		results <- task
 	}
-}
-
-func fillTasks(tasks chan<- int) {
 }
 
 func main() {
@@ -64,18 +67,33 @@ func main() {
 	// Make a worker pool
 	const numTasks = 100
 
-	tasks := make(chan int, numTasks)
-	results := make(chan int, numTasks)
+	tasks := make(chan string, numTasks)
+	results := make(chan string, numTasks)
 
 	// Note that numWorker really depends on machine specs
-	const numWorkers = 1
-	for i := 0; i < numWorkers; i++ {
-		go worker(tasks, results)
+	const numWorkers = 3
+	for i := 1; i <= numWorkers; i++ {
+		go worker(i, tasks, results)
 	}
 
 	for {
-		result := <-results
-		fmt.Println(result)
+		msgRes, err := getMessages(sess, *urlRes.QueueUrl, 10)
+		if err != nil {
+			fmt.Println("Unknown err ", err)
+		}
+
+		if len(msgRes.Messages) == 0 {
+			// fmt.Println("no message")
+			continue
+		}
+
+		msg := *msgRes.Messages[0].Body
+		tasks <- msg
+
+		time.Sleep(time.Second)
+
+		// result := <-results
+		// fmt.Println(result)
 	}
 
 	// msgRes, err := getMessages(sess, *urlRes.QueueUrl, 1)
